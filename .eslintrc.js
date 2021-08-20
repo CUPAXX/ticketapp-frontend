33ae6fb1b7602264e87fa2d13ac9317194dc7db3
cupaxx <fiqri.fa42@gmail.com>
2021-08-20 21:32:14 +0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: ['plugin:react/recommended'],
   parserOptions: {
     requireConfigFile: false,
+    allowImportExportEverywhere: true,
     babelOptions: {
       presets: ['@babel/preset-react'],
     },
